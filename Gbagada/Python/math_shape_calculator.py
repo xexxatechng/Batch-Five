586822fcec45031544d322e9d482489f18a8ebee
xexxatechng <43476648+xexxatechng@users.noreply.github.com>
2018-12-15 00:32:02 +0100

    Create math_shape_calculator.py

---
+++ Gbagada/Python/math_shape_calculator.py
@@ -0,0 +1,24 @@
+ #calculator to calculate the area of simple shapes.
+
+print("Calculator to calculate the area of Simple shapes")
+
+shape = input("select shape:\n(a)triangle \n(b)square \n(c)rectangle \n(d)parallelogram \n")
+if(shape == "a"):
+       import CALCULATEAREAOFATRIANGLE
+       
+elif (shape == "b"):
+      import CALCULATEAREAOFASQUARE
+
+elif (shape == "c"):
+       import CALCULATEAREAOFARECTANGLE
+
+else:
+       import CALCULATEAREAOFAPARALLELOGRAM
+
+
+
+
+
+
+
+
